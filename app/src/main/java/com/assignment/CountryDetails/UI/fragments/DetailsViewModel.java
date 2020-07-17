889a185ec0ab93fb79b6cb31ca9267cbccbbf1a8
sneha zanzane <sneha.zanzane@gmail.com>
2020-07-17 19:51:10 +0530

    feature : 1. Details view removed

--- app/src/main/java/com/assignment/CountryDetails/UI/fragments/DetailsViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.assignment.CountryDetails.UI.fragments;
-
-import androidx.lifecycle.ViewModel;
-
-public class DetailsViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
