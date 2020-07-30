1e4320abeb8f4a864061b5d88fb19cc8fb83f9bb
sneha zanzane <sneha.zanzane@gmail.com>
2020-07-30 18:00:07 +0530

    Unit test cases added for list data

--- app/src/test/java/com/assignment/CountryDetails/ExampleUnitTest.java
+++ app/src/test/java/com/assignment/CountryDetails/ExampleUnitTest.java
@@ -1,17 +1,33 @@
 package com.assignment.CountryDetails;
 
+import com.assignment.CountryDetails.UI.activities.MainActivity;
+import com.assignment.CountryDetails.UI.fragment.ListFragment;
+import com.assignment.CountryDetails.data.adapter.CountryDetailsAdapter;
+import com.assignment.CountryDetails.data.models.CountrySingleton;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+
 }
